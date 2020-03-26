9440c40347cce5243ea6a7f3687a5f2d2b13e9d0
Levan Sarchimelia <levan.sarchimelia97@gmail.com>
2020-03-27 03:27:46 +0400

    tcp redirect fix

--- aws.sh
+++ aws.sh
@@ -11,6 +11,6 @@ git clone https://github.com/GIVIl3o/Bachelors-backend.git
 cd Bachelors-backend
 
 # forward tcp, i'm lazy to do something more complicated :(((
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 
 sudo ./mvnw spring-boot:run
